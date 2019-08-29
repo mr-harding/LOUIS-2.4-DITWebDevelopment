5b7b54f295f50612ebd92ea91136f0bf3f058651
Louis Conway <louis.conway123@gmail.com>
2019-08-30 10:49:04 +1200

    Java Script intergration with google sheet API is working on a single page.

---
+++ code/script.js
@@ -0,0 +1,21 @@
+const Http = new XMLHttpRequest();
+const url='https://sheets.googleapis.com/v4/spreadsheets/1alI8p4CO-AQGIBMDOpSXfEl5qRRUrZEfzA6iE0mN7mQ/values/Sheet1?key=AIzaSyB2Pizd0hheic67F_02cVQJxnArIlrywgo';
+Http.open("GET", url);
+Http.send();
+
+var x;
+
+Http.onreadystatechange = (e) => {
+  x = JSON.parse(Http.responseText);
+
+  // D-house
+  document.getElementById("d-point").innerHTML = x.values[0][1];
+  document.getElementById("d-upcomming").innerHTML = x.values[0][2];
+
+  // A-house
+  document.getElementById("a-point").innerHTML = x.values[1][1];
+  document.getElementById("a-upcomming").innerHTML = x.values[1][2];
+
+  // H-house
+
+}
