6ea11b7ea9314e9b152abc59792a34d66096a75d
Louis Conway <louis.conway123@gmail.com>
2019-09-09 12:00:55 +1200

    Updates google sheet array number so now the correct information is being put on the correct webpages in the correct location

--- script.js
+++ script.js
@@ -15,28 +15,34 @@ function pageSelection(a){
 
   location.replace("house-page.html");
 
-  localStorage.setItem("test", x.values[a][1]);
-  localStorage.setItem("test2", x.values[a][1]);
-  localStorage.setItem("test3", x.values[a][1]);
-  localStorage.setItem("test4", x.values[a][1]);
-  localStorage.setItem("test5", x.values[a][1]);
+  localStorage.setItem("houseName", x.values[a][1]);
+  localStorage.setItem("blurb", x.values[a][2]);
+  localStorage.setItem("point", x.values[a][3]);
+  localStorage.setItem("point-blurb", x.values[a][4]);
+  localStorage.setItem("upcomming", x.values[a][5]);
 
 }
 
 function redirrectOverall(){
+  location.replace("overall.html");
 
-  window.location.href = "overall.html";
+  localStorage.setItem("test6", x.values[7][1]);
+  localStorage.setItem("test7", x.values[8][1]);
+  localStorage.setItem("test7", x.values[9][1]);
+  localStorage.setItem("test9", x.values[10][1]);
+  localStorage.setItem("test10", x.values[11][1]);
 
-  document.getElementById("n1-house").innerHTML = x.values[7][1];
-  document.getElementById("n2-house").innerHTML = x.values[8][1];
-  document.getElementById("n3-house").innerHTML = x.values[9][1];
-  document.getElementById("n4-house").innerHTML = x.values[10][1];
-  document.getElementById("n5-house").innerHTML = x.values[11][1];
 }
 
 
-document.getElementById("house-name").innerHTML = localStorage.getItem("test");
-document.getElementById("house-blurb").innerHTML = localStorage.getItem("test2");
-document.getElementById("point").innerHTML = localStorage.getItem("test3");
-document.getElementById("point-blurb").innerHTML = localStorage.getItem("test4");
-document.getElementById("upcomming").innerHTML = localStorage.getItem("test5");
+document.getElementById("house-name").innerHTML = localStorage.getItem("houseName");
+document.getElementById("house-blurb").innerHTML = localStorage.getItem("blurb");
+document.getElementById("point").innerHTML = localStorage.getItem("point");
+document.getElementById("point-blurb").innerHTML = localStorage.getItem("point-blurb");
+document.getElementById("upcomming").innerHTML = localStorage.getItem("upcomming");
+
+document.getElementById("n1House").innerHTML = localStorage.getItem("test6");
+document.getElementById("n2House").innerHTML = localStorage.getItem("test7");
+document.getElementById("n3House").innerHTML = localStorage.getItem("test8");
+document.getElementById("n4House").innerHTML = localStorage.getItem("test9");
+document.getElementById("n5House").innerHTML = localStorage.getItem("test10");
