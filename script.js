388cd9418055853a33ed825e104fd3a2e860b08a
Louis Conway <louis.conway123@gmail.com>
2019-09-15 11:49:17 +1200

    Working overall page, still loading order issue present, under development

--- script.js
+++ script.js
@@ -31,23 +31,22 @@ function pageSelection(a){
 function redirrectOverall(){
   location.replace("overall.html");
 
-  localStorage.setItem("test6", x.values[7][1]);
-  localStorage.setItem("test7", x.values[8][1]);
-  localStorage.setItem("test7", x.values[9][1]);
-  localStorage.setItem("test9", x.values[10][1]);
-  localStorage.setItem("test10", x.values[11][1]);
+  localStorage.setItem("house1", x.values[7][1]);
+  localStorage.setItem("house2", x.values[8][1]);
+  localStorage.setItem("house3", x.values[9][1]);
+  localStorage.setItem("house4", x.values[10][1]);
+  localStorage.setItem("house5", x.values[11][1]);
 
 }
 
+document.getElementById("n1House").innerHTML = localStorage.getItem("house1");
+document.getElementById("n2House").innerHTML = localStorage.getItem("house2");
+document.getElementById("n3House").innerHTML = localStorage.getItem("house3");
+document.getElementById("n4House").innerHTML = localStorage.getItem("house4");
+document.getElementById("n5House").innerHTML = localStorage.getItem("house5");
 
 document.getElementById("house-name").innerHTML = localStorage.getItem("houseName");
 document.getElementById("house-blurb").innerHTML = localStorage.getItem("blurb");
 document.getElementById("point").innerHTML = localStorage.getItem("point");
 document.getElementById("point-blurb").innerHTML = localStorage.getItem("point-blurb");
 document.getElementById("upcomming").innerHTML = localStorage.getItem("upcomming");
-
-document.getElementById("n1House").innerHTML = localStorage.getItem("test6");
-document.getElementById("n2House").innerHTML = localStorage.getItem("test7");
-document.getElementById("n3House").innerHTML = localStorage.getItem("test8");
-document.getElementById("n4House").innerHTML = localStorage.getItem("test9");
-document.getElementById("n5House").innerHTML = localStorage.getItem("test10");
