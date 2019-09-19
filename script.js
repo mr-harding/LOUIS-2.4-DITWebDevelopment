76ed522ddc3a3b3f448d481efeb3f47523f0e185
Louis Conway <louis.conway123@gmail.com>
2019-09-19 15:16:36 +1200

    code working

--- script.js
+++ script.js
@@ -16,8 +16,8 @@ function pageSelection(a){
   localStorage.setItem("blurb", x.values[a][2]);
   localStorage.setItem("point", x.values[a][3]);
   localStorage.setItem("pointBlurb", x.values[a][4]);
-  localStorage.setItem("upcomming", x.values[a][5]);
-  localStorage.setItem("upcommingBlurb", x.values[a][6]);
+  localStorage.setItem("upcoming", x.values[a][5]);
+  localStorage.setItem("upcomingBlurb", x.values[a][6]);
 
 
 }
@@ -27,5 +27,5 @@ document.getElementById("houseName").innerHTML = localStorage.getItem("houseName
 document.getElementById("houseBlurb").innerHTML = localStorage.getItem("blurb");
 document.getElementById("point").innerHTML = localStorage.getItem("point");
 document.getElementById("pointBlurb").innerHTML = localStorage.getItem("pointBlurb");
-document.getElementById("upcomming").innerHTML = localStorage.getItem("upcomming");
-document.getElementById("upcommingBlurb").innerHTML = localStorage.getItem("upcommingBlurb");
+document.getElementById("upcoming").innerHTML = localStorage.getItem("upcoming");
+document.getElementById("upcomingBlurb").innerHTML = localStorage.getItem("upcomingBlurb");
