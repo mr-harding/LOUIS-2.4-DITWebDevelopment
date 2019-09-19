b2dde0e75d59c3838ca3519e85bb01d27409d384
Louis Conway <louis.conway123@gmail.com>
2019-09-19 20:39:34 +1200

    final commit

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ function pageSelection(a){
   var a = a
   location.replace("house-page.html");
 
+// storing cell information
   localStorage.setItem("houseName", x.values[a][1]);
   localStorage.setItem("blurb", x.values[a][2]);
   localStorage.setItem("point", x.values[a][3]);
@@ -19,13 +20,28 @@ function pageSelection(a){
   localStorage.setItem("upcoming", x.values[a][5]);
   localStorage.setItem("upcomingBlurb", x.values[a][6]);
 
+// background colour selection based on house selected
+  if (a == 1) {
+    localStorage.setItem("houseColour", "black");
+  } else if (a == 2) {
+    localStorage.setItem("houseColour", "blue");
+  } else if (a == 3){
+    localStorage.setItem("houseColour", "green");
+  } else if (a == 4){
+    localStorage.setItem("houseColour", "orange");
+  } else if (a == 5){
+    localStorage.setItem("houseColour", "red");
+  }
 
 }
 
-
+// House Page Spreadsheet information display
 document.getElementById("houseName").innerHTML = localStorage.getItem("houseName");
 document.getElementById("houseBlurb").innerHTML = localStorage.getItem("blurb");
 document.getElementById("point").innerHTML = localStorage.getItem("point");
 document.getElementById("pointBlurb").innerHTML = localStorage.getItem("pointBlurb");
 document.getElementById("upcoming").innerHTML = localStorage.getItem("upcoming");
 document.getElementById("upcomingBlurb").innerHTML = localStorage.getItem("upcomingBlurb");
+
+// House background colour
+document.getElementById("changingColour").style.background = localStorage.getItem("houseColour")
